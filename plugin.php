e07c1cc3afde3c0fdb05479a92078ff075a42c53
jpmanigand <74361467+jpmanigand@users.noreply.github.com>
2024-07-23 14:41:52 +0200

    Update plugin.php
    Prise en compte de la V2.0 dans le fichier de description

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 
 $id = "sync";
-$version = "1.0";
-$ilias_min_version = "6.0";
-$ilias_max_version  = "7.999";
+$version = "2.0";
+$ilias_min_version = "8.12";
+$ilias_max_version  = "8.99";
 $responsible        = "Pôle Pédagogie de la Marine";
 $responsible_mail   = "jeanpaul.manigand@gmail.com";
 $supports_cli_setup = true;
